137912542dd9bd68ae73bafadf39241be3746640
Cedricium <amaya.cedric@gmail.com>
2018-07-19 01:45:45 -0700

    chore: copy only needed files, not directories
    Problem:
    Previously, entire vendor directories were being copied for use in the
    extension. This would result in many unneeded files being flagged
    during the extension upload process.
    
    Solution:
    Rather than copy entire directories, only include the files necessary to
    maintain the extension's functionality. This commit also corrects the
    path to the copied files in the appropriate HTML and CSS files.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,14 +1,13 @@
 #! /usr/bin/env node
 
-// TODO: copy only needed files rather than entire directories
-
 const {copySync} = require('fs-extra');
 
 const files = [
-  copySync('node_modules/animate.css', 'src/public/vendor/animate.css'),
-  copySync('node_modules/bulma', 'src/public/vendor/bulma'),
-  copySync('node_modules/dompurify', 'src/public/vendor/dompurify'),
-  copySync('node_modules/font-awesome', 'src/public/vendor/font-awesome'),
+  copySync('node_modules/animate.css/animate.css', 'src/public/vendor/styles/animate.css'),
+  copySync('node_modules/bulma/', 'src/public/vendor/styles/bulma'),
+  copySync('node_modules/font-awesome/css/font-awesome.min.css', 'src/public/vendor/styles/font-awesome.min.css'),
+  copySync('node_modules/font-awesome/fonts/', 'src/public/vendor/fonts/'),
+  copySync('node_modules/dompurify/dist/purify.min.js', 'src/public/vendor/scripts/purify.min.js'),
 ];
 
 Promise.all(files).catch(err => {
