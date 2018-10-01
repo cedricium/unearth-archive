de362cdeea246dcd88febd617ee06513d89c00e6
Cedricium <amaya.cedric@gmail.com>
2018-10-01 09:00:42 -0700

    enable ssl redirection

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,5 +3,8 @@ module.exports = {
     // { src: 'bulma/bulma.sass', lang: 'sass' },
     // { src: 'font-awesome/scss/font-awesome.scss', lang: 'scss' }
   ],
+  serverMiddleware: [
+    'redirect-ssl'
+  ],
   srcDir: 'src/'
 };
