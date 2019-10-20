c365d8fa1e15550cd2e118c4c0242c60a0aff039
Cedricium <amaya.cedric@gmail.com>
2019-10-20 11:14:29 -0700

    Feat: implement nodemailer transporter using Sendgrid

--- app.js
+++ app.js
@@ -9,20 +9,33 @@ const app = express()
 // Example found from the below Scotch.io blog post:
 // https://scotch.io/tutorials/nodejs-cron-jobs-by-examples#toc-use-case-3-sending-emails-every-n-time-interval
 cron.schedule('* * * * *', async () => {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount()
+  let transporter
+  if (process.env.NODE_ENV !== 'production') {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount()
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  })
+    // create reusable transporter object using the default SMTP transport
+    transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    })
+  } else {
+    transporter = nodemailer.createTransport({
+      host: process.env.SENDGRID_SMTP_HOST,
+      port: process.env.SENDGRID_SMTP_PORT,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.SENDGRID_SMTP_USERNAME,
+        pass: process.env.SENDGRID_SMTP_PASSWORD,
+      },
+    })
+  }
 
   // get data from DB
   const data = await db
@@ -53,9 +66,11 @@ cron.schedule('* * * * *', async () => {
   console.log('Message sent: %s', info.messageId)
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  if (process.env.NODE_ENV !== 'production') {
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
 })
 
 app.get('/', (req, res) => {
