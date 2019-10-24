e5c29f71c8d418da7caeecdaad332ce57af539e3
Cedricium <amaya.cedric@gmail.com>
2019-10-23 21:07:41 -0700

    Initial start at migrating from sqlite to postgres

---
+++ database/migrations/20191023205925_create-tables.js
@@ -0,0 +1,47 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('users', table => {
+      table.increments()
+      table.string('username').notNullable()
+      table
+        .string('email')
+        .unique()
+        .notNullable()
+      table
+        .string('frequency')
+        .default('daily')
+        .notNullable()
+    })
+    .createTable('things', table => {
+      table
+        .string('id')
+        .primary()
+        .unique()
+        .notNullable()
+      table.string('subreddit')
+      table.string('selftext')
+      table.string('author_fullname')
+      table.string('title')
+      table.string('subreddit_name_prefixed')
+      table.string('name')
+      table.string('category')
+      table.integer('score')
+      table.string('thumbnail')
+      table.boolean('over_18')
+      table.string('author')
+      table.string('permalink')
+      table.string('url')
+      table.timestamp('created_utc')
+      table.boolean('surfaced').default(false)
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('users.id')
+        .onDelete('CASCADE')
+    })
+}
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('things').dropTableIfExists('users')
+}
