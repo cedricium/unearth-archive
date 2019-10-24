567d88228f3b070354561ade06fa4fbf8d9106ad
Cedricium <amaya.cedric@gmail.com>
2019-10-24 16:42:58 -0700

    Update name for database url environment variable

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'pg',
     useNullAsDefault: true,
-    connection: process.env.DB_CONNECTION_URI,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './database/migrations',
     },
