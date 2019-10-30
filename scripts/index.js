e6c441987c69e59e657528d4fe69c11f72d8c627
Cedricium <amaya.cedric@gmail.com>
2019-10-29 20:42:45 -0700

    Initial start at implementing a featured thing
    A featured thing simply shows an image for the first of the five surfaced
    things in the email.

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,49 @@
 const db = require('../database/config')
 const data = require('../res/saved_things_ced.json')
 
+/**
+ * Function to determine whether or not a given property exists in a nested object.
+ * @param {Object} object JavaScript object to test against
+ * @param {*} level the desired-level property to begin the search
+ * @param  {...any} rest the remaining deeper-levels to search in
+ * @returns {Boolean} - true if property exists in the given levels, false otherwise
+ *
+ * References: https://stackoverflow.com/a/2631198/6698029
+ */
+const hasOwnNestedProperty = (object, level, ...rest) => {
+  if (object === undefined) return false
+  if (rest.length === 0 && object.hasOwnProperty(level)) return true
+  return hasOwnNestedProperty(object[level], ...rest)
+}
+
+/**
+ * Function that corrects the broken URLs given for images in the Reddit saves JSON.
+ * `&amp;` is used literally in the URLs which then result in HTTP 403, however,
+ * simply using the `&` character in the query parameters fixes the issue.
+ * @param {String} url URL of an image hosted on `external-preview.redd.it`
+ */
+const fixBrokenLink = url => {
+  if (!url) return null
+  return url.replace(/amp;/g, '')
+}
+
 const insertSavesToDB = async () => {
   try {
     const { children } = data.data
     await db('things').insert(
       children.map(child => {
-        const isUrl = new RegExp(/htt(p|ps):\/\//)
         return {
           id: child.data.id,
           subreddit: child.data.subreddit,
           selftext: child.data.selftext,
           author_fullname: child.data.author_fullname,
-          title: child.data.title,
+          title: child.data.link_title || child.data.title,
           subreddit_name_prefixed: child.data.subreddit_name_prefixed,
           name: child.data.name,
           category: child.data.category,
-          thumbnail: isUrl.test(child.data.thumbnail)
-            ? child.data.thumbnail
-            : '',
+          thumbnail: hasOwnNestedProperty(child.data, 'preview', 'images')
+            ? fixBrokenLink(child.data.preview.images[0].source.url)
+            : null,
           author: child.data.author,
           permalink: child.data.permalink,
           url: child.data.url,
