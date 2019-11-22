be1afd0c020d3a78e288e6a5194da1c2de7ecc14
Cedricium <amaya.cedric@gmail.com>
2019-11-21 17:57:53 -0800

    Restructure Express app for socket.io capability

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const apiBaseRouter = require('./api')
+
+app.use('/api/v1', apiBaseRouter)
 
 app.get('/', (req, res) => {
   res.send(`${new Date().toLocaleString('en-US')} - Welcome! 👋`)
--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 require('dotenv').config()
-const server = require('./app')
+const app = require('./app')
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
 const PORT = process.env.PORT || 5000
+
+const RedditService = require('./services/reddit')
+
+io.on('connection', socket => {
+  console.log(`[socket] client connected! - ${socket.id}`)
+  socket.on('check-sync-status', async data => {
+    console.log(`[socket] check-sync-status`)
+    const userSyncStatus = await RedditService.checkSyncStatus(data)
+    socket.emit('sync-status', userSyncStatus)
+  })
+
+  socket.on('sync-reddit-saves', async data => {
+    // moved up and instantiated in `socket.on('connection')`
+    const rs = new RedditService({
+      id: 'a9cqu',
+      username: 'Cedricium',
+      refreshToken: '17232582-g73iP6YNiVvMhf7XWB1hZ3AufiQ',
+    })
+
+    const userSyncStatus = await rs.syncRedditSaves()
+    socket.emit('sync-status', userSyncStatus)
+  })
+})
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
