1efc2ad257adfca53d2d8c0a4b03f94266e9e816
Cedricium <amaya.cedric@gmail.com>
2019-11-25 20:59:33 -0800

    Add `body-parser`, `cors`, and `helmet` packages

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
 const apiBaseRouter = require('./api')
 
+app.use(cors())
+app.use(helmet())
+app.use(bodyParser.json())
 app.use('/api/v1', apiBaseRouter)
 
 app.get('/', (req, res) => {
