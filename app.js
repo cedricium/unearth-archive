8993f50779c047f0697d840c9eb5b7e2ab5cddf7
Cedricium <amaya.cedric@gmail.com>
2019-12-02 22:09:08 -0800

    Use RegExp to test if origin comes from "tryunearth.com"

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const apiBaseRouter = require('./api')
 const corsOptions = {
   origin: (origin, callback) => {
     if (process.env.NODE_ENV === 'production') {
-      if (origin === 'https://www.tryunearth.com/') {
+      // Test to ensure `origin` is the true hostname, subdomain, or
+      // subdirectory of our client's "tryunearth.com" website. To see this
+      // regular expression in action, visit: https://regexr.com/4pvv3
+      const allowedOrigin = RegExp(/tryunearth\.com($|\/+)/gi)
+      if (allowedOrigin.test(origin)) {
         callback(null, true)
       } else {
         callback(`Origin not allowed by CORS`)
