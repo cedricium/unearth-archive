8c0d3da9207d35987d72fb2736d09f69e1761333
Cedricium <amaya.cedric@gmail.com>
2020-01-02 17:25:20 -0800

    Initial stab at refactoring mailing service
    This commit includes the changes for the refactored mailing service.
    This refactored form uses abstractions to reduce the amount of redundant
    code previously introduced when said service was originally implemented.
    
    This commit also includes the updated jobs that use the new mailers, as
    well as the updated and new email templates (newsletter and welcome emails).

--- jobs/sendDailyEmails.js
+++ jobs/sendDailyEmails.js
@@ -1,4 +1,29 @@
-const MailerService = require('../services/mailer')
 const FREQUENCY = 'daily'
+const db = require('../database/config')
+const sendRecurringEmails = require('../mailers/sendRecurringEmails')
 
-MailerService.sendMail(FREQUENCY)
+const run = async () => {
+  const users = await db.table('users').where({ frequency: FREQUENCY })
+  for (let user of users) {
+    const data = await db
+      .table('things')
+      .where({
+        user_id: user.id,
+        surfaced: false,
+      })
+      .whereNotNull('title')
+      .limit(5)
+    await sendRecurringEmails(user.email, data)
+    for (let d of data) {
+      await db('things')
+        .where({ id: d.id })
+        .update({ surfaced: true })
+    }
+  }
+  await db.destroy()
+}
+run().catch(error => {
+  console.log(error)
+  db.destroy()
+  process.exit(1)
+})
