6a142a443ef6c542a2c32d24ea0cce074bad6369
Cedricium <amaya.cedric@gmail.com>
2020-01-29 15:27:13 -0800

    Fix: separate landing and app styles
    This fixes issue #13, applying the Water.css only to the onboarding
    components while keeping the landing component styled the way it was
    intended.

---
+++ client/src/components/Onboarding/style.js
@@ -0,0 +1,5 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const OnboardingGlobalStyle = createGlobalStyle`
+  @import url('https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css');
+`
