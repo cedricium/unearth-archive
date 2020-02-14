4f8a3f0514c47f9fd5d50cd914f3e17f4acc230f
Cedricium <amaya.cedric@gmail.com>
2020-02-13 21:03:08 -0800

    Initial start at re-redesign

--- client-v2/src/pages/index.js
+++ client-v2/src/pages/index.js
@@ -1,94 +1,179 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
-import Footer from '../components/footer'
+import styled, { createGlobalStyle, css } from 'styled-components'
+import { GitHub, Twitter, Users } from 'react-feather'
+
 import Image from '../components/image'
 import SEO from '../components/seo'
 import SendToReddit from '../components/oauth/send-to-reddit'
 
-import {
-  IndexGlobalStyle,
-  Section,
-  SectionWrapper,
-  HeroContent,
-  ImageWrapper,
-  Title,
-  Subheading,
-  ExternalLink,
-  LinkButton,
-  FeatureContent,
-  Header,
-  Text,
-  HorizontalRule,
-} from '../styles/index.styles'
+import {} from '../styles/index.styles'
+
+const IndexGlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'CircularStd', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    color: #000000;
+  }
+`
+
+const Main = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+`
+
+const Section = styled.section`
+  flex-grow: 1;
+  flex-basis: 0;
+  flex-shrink: 0;
+`
+
+const Hero = styled(Section)`
+  padding: 0 0 0 40px;
+  min-width: 720px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  @media (max-width: 720px) {
+    min-width: unset;
+    width: 100%;
+  }
+
+  @media (max-width: 680px) {
+    padding: 60px 10px 0;
+  }
+`
+
+const BackgroundSection = styled(Section)`
+  background: #ff5c00;
+
+  @media (max-width: 720px) {
+    flex-grow: 0;
+  }
+`
+
+const Header = styled.h1`
+  margin: 0 0 20px;
+  max-width: 470px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 46px;
+  line-height: 58px;
+  color: #000000;
+`
+
+const Subheader = styled.p`
+  margin: 0;
+  max-width: 464px;
+  font-style: normal;
+  font-weight: 100;
+  font-size: 24px;
+  line-height: 36px;
+  color: #000000;
+`
+
+const Button = styled.a`
+  display: inline-flex;
+  padding: 10px 20px;
+  border-radius: 10px;
+  text-decoration: none;
+  color: #000000;
+  font-weight: 100;
+
+  ${props =>
+    props.primary &&
+    css`
+      background: #ff5c00;
+      color: #ffffff;
+    `}
+
+  ${props =>
+    props.link &&
+    css`
+      text-decoration: underline;
+      text-decoration-style: dotted;
+    `}
+`
+
+const Footer = styled.footer`
+  padding: 0 10px 0 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 14px;
+
+  & a {
+    margin: 0 0 0 20px;
+    color: #000000;
+    text-decoration-style: dotted;
+  }
+
+  @media (max-width: 680px) {
+    flex-direction: column;
+  }
+`
 
 const IndexPage = () => (
-  <Layout hideHeader hideFooter>
+  <Main>
     <SEO title='Home' />
     <IndexGlobalStyle />
-    <Section fullHeight>
-      <SectionWrapper justifyContent={'space-between'}>
-        <HeroContent>
-          <ImageWrapper width='195px' margin='0'>
-            <Image filename='unearth-logo.png' />
-          </ImageWrapper>
-          <Title>Stop forgetting about your Reddit saves</Title>
-          <Subheading>
-            Receive personalized newsletters with a handful of your Reddit saves
-            at the interval of your choosing and rediscover all the amazing
-            content you’ve forgotten about.
-          </Subheading>
-          <SendToReddit
-            component={LinkButton}
-            value='Get Started Using Reddit'
-          />
-        </HeroContent>
-        <ImageWrapper maxWidth='390px'>
-          <Image filename='115.png' />
-        </ImageWrapper>
-      </SectionWrapper>
-    </Section>
-    <HorizontalRule />
-
-    <Section>
-      <SectionWrapper>
-        <FeatureContent>
-          <Header>Manage your saved content</Header>
-          <Text>
-            All links in your personalized newsletter link directly to the
-            Reddit post or comment you saved, making it easy to unsave things
-            you no longer wish to keep.
-          </Text>
-          <SendToReddit
-            component={ExternalLink}
-            value='Try now using Reddit &rarr;'
-          />
-        </FeatureContent>
-        <ImageWrapper width='500px'>
-          <Image filename='176.png' />
-        </ImageWrapper>
-      </SectionWrapper>
-      <SectionWrapper>
-        <FeatureContent style={{ order: 1 }}>
-          <Header>Go at your own pace</Header>
-          <Text>
-            Choose between receiving daily, weekly, or monthly emails. Perfect
-            for the person who has a lot of saves and wants to sift through them
-            quickly or the person who just likes delaying instant gratification.
-          </Text>
-          <SendToReddit
-            component={ExternalLink}
-            value='Try now using Reddit &rarr;'
-          />
-        </FeatureContent>
-        <ImageWrapper width='500px'>
-          <Image filename='045.png' />
-        </ImageWrapper>
-      </SectionWrapper>
-    </Section>
-
-    <Footer />
-  </Layout>
+    <Hero>
+      <header style={{ maxWidth: '236px', margin: '0 0 40px' }}>
+        <Image filename='unearth-logo.png' />
+      </header>
+      <article>
+        <Header>Stop forgetting about your Reddit saves</Header>
+        <Subheader>
+          Receive personalized newsletters with a handful of your Reddit saves
+          at the interval of your choosing and rediscover all the amazing
+          content you’ve forgotten about.
+        </Subheader>
+        <div style={{ margin: '40px 0' }}>
+          <Button primary href=''>
+            Get Started Using Reddit
+          </Button>
+          {` `}
+          <Button link href=''>
+            View the Source Code
+          </Button>
+        </div>
+      </article>
+      <Footer>
+        <div style={{ display: 'inline-flex', alignItems: 'center' }}>
+          <p>© 2020 · Unearth</p>
+          <a href='https://twitter.com/tryunearth'>
+            <Twitter />
+          </a>
+          <a href='https://github.com/cedricium/unearth'>
+            <GitHub />
+          </a>
+          <a href='https://reddit.com/r/tryunearth'>
+            <Users />
+          </a>
+        </div>
+        <div>
+          <Link to='/acknowledgements'>Acknowledgements</Link>
+          <Link to='/privacy'>Privacy Policy</Link>
+          <Link to='/terms'>Terms of Service</Link>
+        </div>
+      </Footer>
+    </Hero>
+    <BackgroundSection />
+  </Main>
 )
 
 export default IndexPage
