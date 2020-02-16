f47a37a74b16d4b44811bc8c6272578a55641046
Cedric Amaya <amaya.cedric@gmail.com>
2020-02-16 03:23:50 -0800

    SEO Updates (#20)
    * Update SEO component to follow new React Helmet format

--- client-v2/src/components/seo.js
+++ client-v2/src/components/seo.js
@@ -26,61 +26,45 @@ function SEO({ description, lang, meta, title }) {
   )
 
   const metaDescription = description || site.siteMetadata.description
+  const siteUrl = 'https://www.tryunearth.com'
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: `og:url`,
-          content: `https://www.tryunearth.com/`,
-        },
-        {
-          property: `og:image`,
-          content: `https://www.tryunearth.com/og-image.png`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary_large_image`,
-        },
-        {
-          name: `twitter:creator`,
-          content: `@CedricAmaya`,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-        {
-          name: `twitter:image`,
-          content: `https://www.tryunearth.com/og-image.png`,
-        },
-      ]}
-    />
+    <>
+      <Helmet
+        defer={false}
+        defaultTitle={title}
+        titleTemplate={`%s | ${site.siteMetadata.title}`}
+      >
+        <html lang={lang} />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover'
+        />
+
+        {/* General */}
+        <title>{title}</title>
+        <meta name='description' content={metaDescription} />
+        <meta name='image' content={`${siteUrl}/og-image.png`} />
+
+        {/* Open Graph */}
+        <meta property='og:title' content={site.siteMetadata.title} />
+        <meta property='og:type' content='website' />
+        <meta property='og:image' content={`${siteUrl}/og-image.png`} />
+        <meta property='og:url' content={siteUrl} />
+        <meta property='og:description' content={metaDescription} />
+        <meta property='og:locale' content={lang} />
+        <meta property='og:site_name' content={site.siteMetadata.title} />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
+
+        {/* Twitter Card */}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:creator' content='@CedricAmaya' />
+        <meta name='twitter:title' content={site.siteMetadata.title} />
+        <meta name='twitter:description' content={metaDescription} />
+        <meta name='twitter:image' content={`${siteUrl}/og-image.png`} />
+      </Helmet>
+    </>
   )
 }
 
