7441569541e74d949025a3357d3714b1268b4257
Cedricium <amaya.cedric@gmail.com>
2020-02-20 15:34:20 -0800

    Fix: add production property to knexfile

--- knexfile.js
+++ knexfile.js
@@ -29,4 +29,15 @@ module.exports = {
       directory: './database/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
 }
