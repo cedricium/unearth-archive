a8b8c4675f8a45ab92a6791da7f8096b1cc220dc
Cedricium <amaya.cedric@gmail.com>
2020-03-02 22:15:32 -0800

    Fix: use base64url encoding when generating unsubscribe hash
    Problem:
    A user reported haivng issues when trying to use the unsubscribe link
    included in the updated emails. After investigating, I noticed the
    mismatch between the hash in unsubscribe link and the query parameter
    being sent/interpreted on the backend: one had a '+' and the other had
    a ' ' (space) instead.
    
    Solution:
    As outlined in the test case description, a little extra work is needed
    to ensure the encoded hashes are URL-safe. This is accomplished using
    base64url encoding which handles the '+', '/', and '=' characters for us.
    
    References:
     - https://en.wikipedia.org/wiki/Base64#URL_applications

--- tests/utils/generateHash.test.js
+++ tests/utils/generateHash.test.js
@@ -17,4 +17,26 @@ describe('generateHash Helper Function', () => {
     const res = generateHash(emptyString)
     expect(res).not.toHaveLength(0)
   })
+
+  test('should use base64url encoding to generate URL-safe hashes', () => {
+    /**
+     * Standard base64 includes the following characters: + / =
+     *
+     * Using any of those chars in a URL can be costly as they could be
+     * interpreted incorrectly and thus cause functionality dependent upon the
+     * `generateHash` function to fail (as was the case in a user-reported
+     * issue). Thus, we want to use base64url encoding[0] when returning the hash
+     * which maps the problem chars to URL-safe characters for us.
+     *
+     * The email used in this test is a good example: after hashing and standard
+     * base64 is applied, the encoded hash is: 8nWX/aFHt1pQkKqNT+5qrBIEJWzmuArWdlPOb3shBBw=
+     * Note the problem chars as outlined:     ----^------------^-------------------------^
+     *
+     * References:
+     *  [0]: https://en.wikipedia.org/wiki/Base64#URL_applications
+     */
+    const email = 'test_user@gmail.com'
+    const res = generateHash(email)
+    expect(res).not.toMatch(/[\/=+]/)
+  })
 })
--- utils/index.js
+++ utils/index.js
@@ -1,12 +1,13 @@
 const hmacSHA256 = require('crypto-js/hmac-sha256')
 const base64 = require('crypto-js/enc-base64')
+const base64url = require('base64url')
 
 const SECRET = process.env.SECRET || 'secret hash string'
 
 const generateHash = message => {
   const hash = hmacSHA256(message, SECRET)
   const encodedHash = base64.stringify(hash)
-  return encodedHash
+  return base64url.fromBase64(encodedHash)
 }
 
 module.exports = { generateHash }
